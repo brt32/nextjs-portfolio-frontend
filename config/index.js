7e69deb87d83d83a481aaf9c799da1bcd150a254
Bartek <bartlomiej.proczkowski@gmail.com>
2021-08-20 02:15:01 +0200

    Url updated

--- config/index.js
+++ config/index.js
@@ -1,10 +1,10 @@
 export const API_URL =
   process.env.NEXT_PUBLIC_API_URL ||
-  "https://pf-nextjs-backend-strapi.herokuapp.com:1337";
+  "https://pf-nextjs-backend-strapi.herokuapp.com";
 
 export const NEXT_URL =
   process.env.NEXT_PUBLIC_FRONTEND_URL ||
-  "https://pf-nextjs-backend-strapi.herokuapp.com:3000";
+  "https://pf-nextjs-backend-strapi.herokuapp.com";
 
 export const PER_PAGE = 3;
 
