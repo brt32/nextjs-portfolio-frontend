81e57eac43ffba99f26d3388741bee414a3eef37
Bartek <bartlomiej.proczkowski@gmail.com>
2021-09-25 16:56:53 +0200

    Testing footer 4

--- components/Footer.js
+++ components/Footer.js
@@ -3,9 +3,13 @@ import styles from "@/styles/Footer.module.css";
 
 const Footer = () => {
   return (
-    <footer className={styles.about}>
-      <p>Copyright &copy; <Link href="/about"><a className={styles.footer}>B.Proczkowski</a></Link> Portfolio @2021</p>
-    </footer>
+    <p>
+      Copyright &copy;{" "}
+      <Link href="/about">
+        <a className={styles.footer}>B.Proczkowski</a>
+      </Link>{" "}
+      Portfolio @2021
+    </p>
   );
 };
 
