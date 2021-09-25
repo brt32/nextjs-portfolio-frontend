c49db691bcc5bfd770240f82ff54ee98e887644f
Bartek <bartlomiej.proczkowski@gmail.com>
2021-09-25 18:54:22 +0200

    Change Home Page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-import fs from "fs";
-import path from "path";
+// import fs from "fs";
+// import path from "path";
 import matter from "gray-matter";
 import { sortByDate } from "../utils/index";
 import styles from "@/styles/Index.module.css";
@@ -30,49 +30,53 @@ export default function HomePage({ events, posts }) {
           </div>
         )}
 
-        <h1 className={styles.latest}>Latest Blog Posts</h1>
+        {/* <h1 className={styles.latest}>Latest Blog Posts</h1>
         {posts.length === 0 && <h3>No blog posts here...</h3>}
         {posts.map((post, index) => (
           <Post key={index} post={post} />
         ))}
-        {events.length > 0 && (
+        {posts.length > 0 && (
           <div className={styles.btn}>
             <Link href="/blog">
               <a className="btn-secondary">View All Blog Posts</a>
             </Link>
           </div>
-        )}
+        )} */}
       </Layout>
     </motion.div>
   );
 }
 
-export async function getStaticProps() {
+export async function getServerSideProps() {
   const res = await fetch(
     `${API_URL}/events?isUpcoming=true&_sort=startDate:ASC&_limit=3`
   );
   const events = await res.json();
 
-  const files = fs.readdirSync(path.join("posts"));
+  // const files = fs.readdirSync(path.join("posts"));
 
-  const posts = files.map((filename) => {
-    const slug = filename.replace(".md", "");
+  // const posts = files.map((filename) => {
+  //   const slug = filename.replace(".md", "");
 
-    const markdownWithMeta = fs.readFileSync(
-      path.join("posts", filename),
-      "utf-8"
-    );
+  //   const markdownWithMeta = fs.readFileSync(
+  //     path.join("posts", filename),
+  //     "utf-8"
+  //   );
 
-    const { data: frontmatter } = matter(markdownWithMeta);
+  //   const { data: frontmatter } = matter(markdownWithMeta);
 
-    return {
-      slug,
-      frontmatter,
-    };
-  });
+  //   return {
+  //     slug,
+  //     frontmatter,
+  //   };
+  // });
+
+  // return {
+  //   props: { events, posts: posts.sort(sortByDate).slice(0, 3) },
+  //   revalidate: 1,
+  // };
 
   return {
-    props: { events, posts: posts.sort(sortByDate).slice(0, 3) },
-    revalidate: 1,
+    props: { events },
   };
 }
